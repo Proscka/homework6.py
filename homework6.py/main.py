95030cb4b0798ec18fd7d38537a425e37849e66e
Proscka <proskinaviktoria861@gmail.com>
2024-06-26 18:18:01 +0300

    Add files via upload

---
+++ homework6.py/main.py
@@ -0,0 +1,20 @@
+my_dict={"Katya":1994,"Elle":1991,"Sahsa":2012}
+print(my_dict)
+print(my_dict.get("Katya"))
+print(my_dict.get("Jroslav"))
+my_dict.update({"Daniel":2021,
+                "Lev":2024})
+print(my_dict)
+my_dict.pop("Elle")
+print(my_dict)
+my_set={1,2,"цифра","цифра",9,10,1,7}
+print(my_set)
+my_set={1,7,"цифра","цифра",9,10,"string",True,(1,7,4,5)}
+print(my_set)
+my_set={1,7,"цифра",9,10,1,7,4,5}
+print(tuple)
+my_set.remove(1)
+print(my_set)
+
+
+
